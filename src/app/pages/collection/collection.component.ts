bb3d64a12b45b104398dd77dc5773486a840f5dc
Lucas Marques <lucasmarqueshzx@gmail.com>
2024-04-04 12:56:34 -0300

    Corrigindo excluir game

--- src/app/pages/collection/collection.component.ts
+++ src/app/pages/collection/collection.component.ts
@@ -36,6 +36,8 @@ export class CollectionComponent implements OnInit {
   removeGame(gameID: any) {
     this.pageCollectionGames = this.pageCollectionGames.filter((game: any) => game.id !== gameID);
     this.pageCollection = this.pageCollection.filter((id: any) => id !== gameID);
+    const collectionObj = this.allCollections.filter((collection: any) => collection.id == this.collectionID);
+    collectionObj[0].collection = this.pageCollection;
     console.log(this.pageCollectionGames, this.pageCollection, this.allCollections)
     this.updateUserCollections();
   }
